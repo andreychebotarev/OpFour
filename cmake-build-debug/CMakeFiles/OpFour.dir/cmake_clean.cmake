e99788b07dc9071066b17aae758364fa02b280a3
Andrey Cheb <andreychebotarev@yahoo.com>
2019-07-18 10:46:15 -0700

    Blue Print

---
+++ cmake-build-debug/CMakeFiles/OpFour.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpFour.dir/main.cpp.o"
+  "OpFour.pdb"
+  "OpFour"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpFour.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
